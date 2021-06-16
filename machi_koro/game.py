f7dcc68cabad3bac5a3e11b04608f26a1703dd09
Daniil Botnarenku <d.botnarenku@innopolis.ru>
2021-06-16 18:05:34 +0300

    wip implementing game play method

--- machi_koro/game.py
+++ machi_koro/game.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from machi_koro.cards.establishment import *
 from machi_koro.reserve import Reserve
 from machi_koro.town import Town
@@ -19,7 +21,32 @@ class Game:
             self.reserve.put(card, 6)
 
     def play(self):
+        winner = None
+        while winner is None:
+            for player in self.players:
+                self.make_turn(player)
+                if self.is_winner(player):
+                    winner = player
+        return winner
+
+    def make_turn(self, player):
         pass
+
+    @staticmethod
+    def throw_dice(count):
+        dice_sum = 0
+        for i in range(count):
+            dice_sum += randint(1, 6)
+        return dice_sum
+
+    @staticmethod
+    def is_winner(player):
+        return (
+            player.town.train_station.built
+            and player.town.shopping_mall.built
+            and player.town.amusement_park.built
+            and player.town.radio_tower.built
+        )
 
     ESTABLISHMENTS = (
         WheatField, Ranch, Forest, Mine, AppleOrchard, Bakery, ConvenienceStore,
